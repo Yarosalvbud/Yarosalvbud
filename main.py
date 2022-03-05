a12ff039f18b5c662b5a01be8dc5bfe3872cce7e
Yarosalvbud <93933897+Yarosalvbud@users.noreply.github.com>
2022-03-05 12:34:23 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-import sys
-import decoding, encoding
+class Lexer:
+    def __init__(self):
+        self.buffer = ""
+        self.b_counter = 0
+        self.current_state = "s"
+        self.lexem = []
+        self.end_m = ['+', '-', '*', '/', '^', '(']
+        self.viraz = []
 
-if len(sys.argv) == 3:
-    name = sys.argv[2]
-    rabota = sys.argv[1]
-    if rabota == '-e':
-        encoding.encode(name)
-        print('got it')
-    else:
-        decoding.decode(name)
-        print('got it -d')
-        
-    
+
+
+    def s_func(self, stroka):
+        for symbol in range(len(stroka)):
+            if stroka[symbol].isdigit():
+                self.current_state = 'I'
+                self.i_func(stroka[symbol])
+            elif stroka[symbol] == '.':
+                self.current_state = 'R'
+                self.r_func(stroka[symbol])
+            else:
+                self.end_func(stroka[symbol])
+
+
+    def i_func(self, symbol):
+        if self.current_state == 'I':
+            self.buffer += symbol
+
+    def r_func(self, symbol):
+        if self.current_state == 'R':
+            self.buffer += symbol
+
+    def end_func(self, symbol):
+        if symbol in self.end_m:
+            self.viraz.append(self.buffer)
+            self.viraz.append(self.end_m[self.end_m.index(symbol)])
+            self.buffer = ""
+
+s = Lexer()
+s.s_func('5.38+5')
+print(s.viraz)
+#Запускаем цикл в parse затем переходим в s_func которая распределит состояние и пришлёт ответ
