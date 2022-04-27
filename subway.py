3d4140be44d7027ce02c419de1d50bef6f147d0e
Yarosalvbud <93933897+Yarosalvbud@users.noreply.github.com>
2022-04-27 09:04:24 +0300

    Add files via upload

--- subway.py
+++ subway.py
@@ -1,39 +1,76 @@
 import asyncio
 import time
 import random
+import os
 
 start_time = time.time()
 trains = asyncio.Queue()
 stations = []
+people_p = []
+people_t = []
+time_a = []
+count_train = 5
 
 
 class Train:
     def __init__(self, number):
-        self.free_place = 200
+        self.free_place = 400
         self.time_go = {'Рокоссовская': 6, 'Соборная': 3, 'Кристалл': 2, 'Заречная': 7,
                         'Библиотека имени Пушкина': 0}
+        self.time_go_b = {'Библиотека имени Пушкина': 7, 'Заречная': 2, 'Кристалл': 3, 'Соборная': 6, 'Рокоссовская': 0}
         self.number = number
         self.current = ''
         self.id = 0
+        self.sight = 0
 
     async def bring_people(self, st):
         people_on_s = st.people
-        self.free_place += random.randint(0, 200 - self.free_place)
-        self.id += 1
-        await st.control(self.number, self.free_place)
-        if people_on_s > self.free_place:
-            self.free_place = 0
-        else:
-            self.free_place = self.free_place - people_on_s
-        await asyncio.sleep(0.15)
-        print('Поезд произвёл посадку и высадку пассажиров')
+        self.current = st.name
         if self.current == 'Библиотека имени Пушкина':
-            print('Поезд завершил свою поездку')
+            self.free_place = 400
+            self.sight = 1
+            self.id = 0
+            print(
+                f'Поезд {self.number} высадил всех пассажиров на станции {self.current} и начинает двигаться в обратную сторону')
+            print(' ')
+        else:
+            self.free_place += random.randint(0, 400 - self.free_place) if self.id != 0 else 0
+            self.id += 1
+            await st.control(self.number, self.free_place)
+            self.free_place = 0 if people_on_s > self.free_place else self.free_place - people_on_s
+            await asyncio.sleep(0.15)
+            print(f'Поезд {self.number} произвёл посадку и высадку пассажиров')
+            print(f"Свободных мест в поезде {self.free_place}")
+            print(' ')
+
+    async def bring_people_b(self, st):
+        people_on_s = st.people_b
+        self.current = st.name
+        if self.current == 'Рокоссовская':
+            self.free_place = 400
+            self.sight = 0
+            self.id = 0
+            print(
+                f'Поезд {self.number} высадил всех пассажиров на станции {self.current} и начинает двигаться в обратную сторону')
+            print(' ')
+        else:
+            self.free_place += random.randint(0, 400 - self.free_place) if self.id != 0 else 0
+            self.id += 1
+            await st.control_b(self.number, self.free_place)
+            self.free_place = 0 if people_on_s > self.free_place else self.free_place - people_on_s
+            await asyncio.sleep(0.15)
+            print(f'Поезд {self.number} произвёл посадку и высадку пассажиров')
+            print(f"Свободных мест в поезде {self.free_place}")
+            print(' ')
 
     async def train_go(self, st):
         print('Поезд движется на следующую станцию')
-        await self.bring_people(st)
-        await asyncio.sleep(self.time_go.get(st.name))
+        if self.sight == 0:
+            await self.bring_people(st)
+            await asyncio.sleep(self.time_go.get(st.name))
+        else:
+            await self.bring_people_b(st)
+            await asyncio.sleep(self.time_go_b.get(st.name))
 
 
 class Station:
@@ -41,34 +78,56 @@ class Station:
     def __init__(self, name):
         self.name = name
         self.people = 0
+        self.people_b = 0
 
     async def control(self, number, free_place):
         print(f"Поезд {number} прибыл на станцию {self.name}")
+        print(f"Людей на станци {self.name} {self.people}")
         self.people = self.people - free_place if self.people > free_place else 0
+        print(f"Людей на станци {self.name} {self.people}")
+
+    async def control_b(self, number, free_place):
+        print(f"Поезд {number} прибыл на станцию {self.name}")
+        print(f"Людей на станци {self.name}--{self.people_b}")
+        self.people_b = self.people_b - free_place if self.people_b > free_place else 0
+        print(f"Людей на станци {self.name}--{self.people_b}")
 
     async def passengers(self):
         while True:
-            self.people += 50
-            await asyncio.sleep(1)
-            print(f"Людей на станци {self.name} {self.people}")
+            if time.time() - start_time >= 18:
+                coin = random.randint(0, 1)
+                if coin == 0 and self.name != 'Библиотека имени Пушкина':
+                    self.people += 1
+                elif coin == 0 and self.name == 'Библиотека имени Пушкина':
+                    self.people_b += 1
+                elif self.name != 'Рокоссовская' and coin == 1:
+                    self.people_b += 1
+                elif self.name == 'Рокоссовская' and coin == 1:
+                    self.people += 1
+                await asyncio.sleep(0.016)
+            else:
+                await asyncio.sleep(18)
 
 
 async def trains_online():
     num = 0
-    while True:
+    for i in range(count_train):
         num += 1
         train = Train(num)
         await trains.put(train)
-        await asyncio.sleep(3)
+        await asyncio.sleep(38 / count_train)
 
 
 async def main():
     while True:
         train = await trains.get()
-        st = stations[train.id]
+        st = stations[train.id] if train.sight == 0 else stations_b[train.id]
         await train.train_go(st)
-        if st.name != 'Библиотека имени пушкина':
-            await trains.put(train)
+        await trains.put(train)
+
+
+async def get_average(people_p, people_t):
+    pass
 
 
 Rok = Station('Рокоссовская')
@@ -81,6 +140,7 @@ stations.append(Sob)
 stations.append(Cry)
 stations.append(Zar)
 stations.append(Lib)
+stations_b = stations[::-1]
 
 loop = asyncio.get_event_loop()
 loop.create_task(Rok.passengers())
@@ -89,5 +149,6 @@ loop.create_task(Cry.passengers())
 loop.create_task(Zar.passengers())
 loop.create_task(Lib.passengers())
 loop.create_task(trains_online())
-loop.create_task(main())
+for i in range(count_train):
+    loop.create_task(main())
 loop.run_forever()
